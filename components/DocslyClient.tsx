26205c36b47796bd43bf8a18fc74b609b4470a43
Anshuman Bhardwaj <anshu5074@gmail.com>
2023-03-17 18:36:05 +0530

    update publicId

--- components/DocslyClient.tsx
+++ components/DocslyClient.tsx
@@ -4,5 +4,10 @@ import { usePathname } from "next/navigation";
 
 export default function DocslyClient() {
   const pathname = usePathname();
-  return <Docsly publicId="asdfasdf" pathname={pathname} />;
+  return (
+    <Docsly
+      publicId="public_vvIoQlAhwLyCIj2VO8IVUA1Ka210rTKYM8rP"
+      pathname={pathname}
+    />
+  );
 }
